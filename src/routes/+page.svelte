6230edc406d5cef5ec6b115aeef95ddf6967dd4e
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2024-03-17 21:00:41 +0500

    Tweak clock styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,90 @@
 </script>
 
 <div class="page">
-    <Clock></Clock>
-
-    <div class="controls">
-        <button class="button glass"> Start! </button>
+    <div class="clock">
+        <Clock></Clock>
     </div>
 
-    <div class="select-task">
-        <button class="button"> Select a task </button>
+    <button class="select-task hidden"> Select a task </button>
+
+    <button class="start-button glass hidden"> Start! </button>
+
+    <div class="active-task">
+        <p class="active-task__label">Working on</p>
+        <p class="active-task__name">Something extremely important</p>
+    </div>
+
+    <div class="controls">
+        <button class="controls__give-up">Give up</button>
+        <button class="controls__finished glass">Finished!</button>
     </div>
 </div>
 
-<style></style>
+<style>
+    /* Tablet first, since it is the simplest layout */
+
+    .page {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 5rem;
+        justify-items: center;
+        align-items: center;
+    }
+
+    .clock {
+        width: 100%;
+        grid-column: 1 / 3;
+    }
+
+    .select-task,
+    .active-task__name {
+        font-size: var(--font-size-600);
+    }
+    .start-button,
+    .controls {
+        font-size: var(--font-size-500);
+    }
+
+    .start-button,
+    .controls__finished {
+        min-height: 3rem;
+        padding-inline: 2rem;
+        box-shadow: 0.25rem 0.25rem 2rem 0.25rem rgba(0, 0, 0, 0.1);
+    }
+
+    .active-task {
+        grid-column: 1 / 3;
+
+        display: grid;
+        justify-items: center;
+    }
+
+    .active-task__label {
+        font-weight: var(--font-weight-bold);
+    }
+
+    .controls {
+        grid-column: 1 / 3;
+
+        display: flex;
+        gap: 3rem;
+    }
+
+    @media screen and (width > 64rem) {
+        .page {
+            gap: 0rem;
+        }
+        .active-task {
+            grid-column: 1 / 2;
+        }
+
+        .controls {
+            grid-column: 1 / 2;
+        }
+
+        .clock {
+            grid-column: 2 / 3;
+            grid-row: 1 / 3;
+        }
+    }
+</style>
--- src/routes/Clock.svelte
+++ src/routes/Clock.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
-    import clockShadowMobile from "../lib/images/clock_shadow_mobile.svg";
-    import clockShadowTablet from "../lib/images/clock_shadow_tablet.svg";
 </script>
 
-<div
-    class="clock"
-    style="
-        --clock-shadow-mobile: url({clockShadowMobile});
-        --clock-shadow-tablet: url({clockShadowTablet});"
->
+<div class="clock">
     <div class="clock__digits">
         <span class="clock__digits">25</span>
         <span class="clock__digits">20</span>
@@ -21,26 +14,18 @@
     <div class="clock__face">
         <div class="hand"></div>
         <div class="clock__ring glass"></div>
+        <div class="clock__controls">
+            <slot />
+        </div>
     </div>
 
     <!-- The SVGs are not displayed and used purely as masks -->
 
-    <svg viewBox="0 0 1 1" class="mask-mobile">
+    <svg viewBox="0 0 1 1" class="mask-mobile sr-only">
         <defs>
             <clipPath clipPathUnits="objectBoundingBox" id="maskMobile">
                 <path
                     d="M1 0.5C1 0.776142 0.776142 1 0.5 1C0.223858 1 0 0.776142 0 0.5C0 0.223858 0.223858 0 0.5 0C0.776142 0 1 0.223858 1 0.5ZM0.11 0.5C0.11 0.715391 0.284609 0.89 0.5 0.89C0.715391 0.89 0.89 0.715391 0.89 0.5C0.89 0.284609 0.715391 0.11 0.5 0.11C0.284609 0.11 0.11 0.284609 0.11 0.5Z"
-                    fill="black"
-                />
-            </clipPath>
-        </defs>
-    </svg>
-
-    <svg viewBox="0 0 1 1" class="mask-tablet">
-        <defs>
-            <clipPath clipPathUnits="objectBoundingBox" id="maskTablet">
-                <path
-                    d="M1 0.5C1 0.776142 0.776142 1 0.5 1C0.223858 1 0 0.776142 0 0.5C0 0.223858 0.223858 0 0.5 0C0.776142 0 1 0.223858 1 0.5ZM0.0984375 0.5C0.0984375 0.721777 0.278223 0.901563 0.5 0.901563C0.721777 0.901563 0.901563 0.721777 0.901563 0.5C0.901563 0.278223 0.721777 0.0984375 0.5 0.0984375C0.278223 0.0984375 0.0984375 0.278223 0.0984375 0.5Z"
                     fill="black"
                 />
             </clipPath>
@@ -54,9 +39,10 @@
 
 <style>
     .clock {
-        max-width: 46vh;
-    }
+        max-width: 60vh;
 
+        padding-inline: 2rem;
+    }
     .clock__face {
         position: relative;
 
@@ -65,38 +51,18 @@
         box-shadow: 0.25rem 0.25rem 2rem 0.25rem rgba(0, 0, 0, 0.1);
     }
 
-    /* Fake shadow inside the ring */
-
-    .clock__face::before {
-        content: "";
-        position: absolute;
-        width: 78%;
-        aspect-ratio: 1;
-        border-radius: 50%;
-
-        transform: translate(-50%, -50%);
-        top: 50%;
-        left: 50%;
-
-        background: radial-gradient(
-            closest-side at center,
-            rgba(0, 0, 0, 0) calc(100% - 2rem),
-            rgba(0, 0, 0, 0.05)
-        );
-    }
-
     .hand {
         position: absolute;
         z-index: 10;
 
         width: 0.25rem;
-        height: 40%;
+        height: 53%;
 
         transform-origin: 50% 102%;
 
         transform: translateX(-50%) rotate(45deg);
         left: 50%;
-        top: 8%;
+        top: -3%;
 
         background-color: var(--color-primary-400);
         box-shadow: 0 0 1rem 0 var(--color-primary-400);
@@ -105,37 +71,34 @@
     .clock__ring {
         border-radius: 50%;
         aspect-ratio: 1;
-
-        clip-path: url(#maskMobile);
     }
 
-    @media screen and (width > 40rem) {
-        .clock {
-            max-width: 60vh;
-
-            padding-inline: 2rem;
+    @media screen and (width < 40rem) {
+        .clock__ring {
+            clip-path: url(#maskMobile);
         }
+
+        /* Fake shadow inside the ring */
 
         .clock__face::before {
-            width: 80%;
-        }
+            content: "";
+            position: absolute;
+            width: 78%;
+            aspect-ratio: 1;
+            border-radius: 50%;
 
-        .clock__ring {
-            clip-path: url(#maskTablet);
+            transform: translate(-50%, -50%);
+            top: 50%;
+            left: 50%;
+
+            background: radial-gradient(
+                closest-side at center,
+                rgba(0, 0, 0, 0) calc(100% - 2rem),
+                rgba(0, 0, 0, 0.05)
+            );
         }
     }
 
     @media screen and (width > 64rem) {
-        .clock__face::before {
-            display: none;
-        }
-
-        .hand {
-            height: 53%;
-            top: -3%;
-        }
-        .clock__ring {
-            clip-path: none;
-        }
     }
 </style>
