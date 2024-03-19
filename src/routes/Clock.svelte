826fd21f5a17239242188e762fca42f5bc7d4b1d
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2024-03-19 17:42:19 +0500

    Finish styling the clock

--- src/routes/Clock.svelte
+++ src/routes/Clock.svelte
@@ -1,23 +1,26 @@
 <script lang="ts">
+    import pointer from "$lib/images/pointer.svg"
 </script>
 
-<div class="clock">
+<div class="clock" style="--pointer: url({pointer});">
+    <div class="glass clock__face"></div>
+
     <div class="clock__digits">
-        <span class="clock__digits">25</span>
-        <span class="clock__digits">20</span>
-        <span class="clock__digits">15</span>
-        <span class="clock__digits">10</span>
-        <span class="clock__digits">5</span>
-        <span class="clock__digits">0</span>
+        <span class="clock__digit clock__digit--1">25</span>
+        <span class="clock__digit clock__digit--2">20</span>
+        <span class="clock__digit clock__digit--3">15</span>
+        <span class="clock__digit clock__digit--4">10</span>
+        <span class="clock__digit clock__digit--5">5</span>
+        <span class="clock__digit clock__digit--0">0</span>
     </div>
 
-    <div class="clock__face">
-        <div class="hand"></div>
-        <div class="clock__ring glass"></div>
-        <div class="clock__controls">
-            <slot />
-        </div>
+    <div class="pointer"></div>
+
+    <div class="clock__controls">
+        <slot />
     </div>
+
+    <div class="hand"></div>
 
     <!-- The SVGs are not displayed and used purely as masks -->
 
@@ -31,59 +34,131 @@
             </clipPath>
         </defs>
     </svg>
-
-    <div class="controls">
-        <slot />
-    </div>
 </div>
 
 <style>
+    /* General styling / clock face */
     .clock {
-        max-width: 60vh;
-
-        padding-inline: 2rem;
-    }
-    .clock__face {
         position: relative;
+        container: clock-face / inline-size;
+
+        width: 100%;
+        max-width: 60vh;
+        aspect-ratio: 1;
 
         border-radius: 50%;
 
+        --_clock-face-diameter: 80cqw;
+    }
+
+    /* Workaround for the clock face shadow which otherwise would have been clipped */
+
+    .clock::before {
+        content: "";
+        position: absolute;
+        width: var(--_clock-face-diameter);
+        aspect-ratio: 1;
+        border-radius: 50%;
+
+        transform: translate(-50%, -50%);
+        top: 50%;
+        left: 50%;
+
         box-shadow: 0.25rem 0.25rem 2rem 0.25rem rgba(0, 0, 0, 0.1);
     }
+
+    .clock__face {
+        position: absolute;
+
+        width: var(--_clock-face-diameter);
+        border-radius: 50%;
+        aspect-ratio: 1;
+
+        transform: translate(-50%, -50%);
+        top: 50%;
+        left: 50%;
+    }
+
+    /* Digits */
+
+    .clock__digits {
+        position: absolute;
+        inset: 0;
+    }
+
+    .clock__digit {
+        position: absolute;
+
+        height: 50cqw;
+
+        padding-block-start: 1.5cqw;
+
+        transform-origin: 50% 100%;
+        left: 50%;
+
+        font-size: 5.4cqw;
+    }
+
+    /* I wish there was a more concise way to do this */
+
+    .clock__digit--0 {
+        transform: translateX(-50%);
+    }
+
+    .clock__digit--1 {
+        transform: translateX(-50%) rotate(-60deg);
+    }
+
+    .clock__digit--2 {
+        transform: translateX(-50%) rotate(-120deg);
+    }
+
+    .clock__digit--3 {
+        transform: translateX(-50%) rotate(-180deg);
+    }
+
+    .clock__digit--4 {
+        transform: translateX(-50%) rotate(-240deg);
+    }
+
+    .clock__digit--5 {
+        transform: translateX(-50%) rotate(-300deg);
+    }
+
+    /* Pointers */
 
     .hand {
         position: absolute;
         z-index: 10;
 
         width: 0.25rem;
-        height: 53%;
+        height: 42cqw;
 
-        transform-origin: 50% 102%;
+        transform-origin: 50% 44cqw;
 
-        transform: translateX(-50%) rotate(45deg);
+        transform: translateX(-50%) rotate(57deg);
         left: 50%;
-        top: -3%;
+        top: 6cqw;
 
         background-color: var(--color-primary-400);
         box-shadow: 0 0 1rem 0 var(--color-primary-400);
     }
 
-    .clock__ring {
-        border-radius: 50%;
-        aspect-ratio: 1;
-    }
-
     @media screen and (width < 40rem) {
-        .clock__ring {
+        .clock {
+            --_clock-face-diameter: 100cqw;
+        }
+
+        .clock__face {
             clip-path: url(#maskMobile);
         }
 
         /* Fake shadow inside the ring */
 
-        .clock__face::before {
+        .clock::after {
             content: "";
             position: absolute;
-            width: 78%;
+            width: 78cqw;
             aspect-ratio: 1;
             border-radius: 50%;
 
@@ -96,6 +171,29 @@
                 rgba(0, 0, 0, 0) calc(100% - 2rem),
                 rgba(0, 0, 0, 0.05)
             );
+        }
+
+        .hand {
+            display: none;
+        }
+
+        /* Notch-style pointer */
+
+        .pointer {
+            position: absolute;
+
+            width: 11cqw;
+            height: 44cqw;
+
+            top: 6cqw;
+            left: 50%;
+
+            transform-origin: 50% 100%;
+            transform: translateX(-50%) rotate(57deg);
+
+            background-image: var(--pointer);
+            background-repeat: no-repeat;
+            background-size: contain;
         }
     }
 
