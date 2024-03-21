ae2008a4b45f945ade33867da1fb16c8cc86bae2
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2024-03-21 22:18:03 +0500

    Style task tiles

--- src/routes/tasks/+page.svelte
+++ src/routes/tasks/+page.svelte
@@ -1,14 +1,51 @@
 <script lang="ts">
-    import NewTaskForm from "./NewTaskForm.svelte";
-    import TasksList from "./TasksList.svelte";
+    import { placeholderData } from "$lib/utils/placeholderData"
+    import NewTaskForm from "./NewTaskForm.svelte"
+    import TaskTile from "./TaskTile.svelte"
 </script>
 
 <div class="page">
-    <div class="form">
+    <div class="glass form">
         <NewTaskForm></NewTaskForm>
     </div>
 
-    <div class="tasks">
-        <TasksList></TasksList>
-    </div>
+    <ul class="tasks-list">
+        {#if placeholderData.length > 0}
+            {#each placeholderData as task}
+                <TaskTile {task}></TaskTile>
+            {/each}
+        {/if}
+    </ul>
 </div>
+
+<style>
+    .page {
+        padding-block: 6.75rem;
+        padding-inline: 2rem;
+        display: grid;
+        gap: 2rem;
+    }
+
+    .form {
+        /* Temporary */
+        display: none;
+    }
+
+    .tasks-list {
+        box-shadow: 0.25rem 0.25rem 2rem 0.25rem rgba(0, 0, 0, 0.1);
+    }
+
+    @media screen and (width > 40rem) {
+        .page {
+            padding-block: 8rem;
+            padding-inline: 4rem;
+            gap: 5rem;
+        }
+    }
+
+    @media screen and (width > 64rem) {
+        .page {
+            padding-inline: 20%;
+        }
+    }
+</style>
--- src/routes/tasks/TaskTile.svelte
+++ src/routes/tasks/TaskTile.svelte
@@ -1,30 +1,28 @@
 <script lang="ts">
-    import type { Task } from "$lib/types/task";
+    import type { Task } from "$lib/types/task"
 
-    import iconExpand from "$lib/images/icon_expand.svg";
-    import iconEdit from "$lib/images/icon_edit.svg";
-    import iconDelete from "$lib/images/icon_delete.svg";
-    import iconClose from "$lib/images/icon_close.svg";
+    import iconExpand from "$lib/images/icon_expand.svg"
+    import iconEdit from "$lib/images/icon_edit.svg"
+    import iconDelete from "$lib/images/icon_delete.svg"
+    import iconClose from "$lib/images/icon_close.svg"
 
-    export let task: Task;
+    export let task: Task
 </script>
 
-<div class="task-tile glass">
-    <div class="task-tile__header">
-        <input type="checkbox" class="hidden" id={task.id} />
+<div class="glass task-tile">
+    <input type="checkbox" class="hidden" id={task.id} />
 
-        <label for={task.id} class="task-tile__checkbox">
-            <span class="sr-only">Mark as completed</span>
-        </label>
+    <label for={task.id} class="task-tile__checkbox">
+        <span class="sr-only">Mark as completed</span>
+    </label>
 
-        <h2 class="task-tile__name">
-            {task.name}
-        </h2>
+    <p class="task-tile__name">
+        {task.name}
+    </p>
 
-        <button class="expand">
-            <img src={iconExpand} alt="expand" class="expand__icon" />
-        </button>
-    </div>
+    <button class="expand">
+        <img src={iconExpand} alt="expand" class="expand__icon" />
+    </button>
 
     <div class="task-tile__expandable">
         <p class="task-tile__description">
@@ -46,3 +44,174 @@
         </div>
     </div>
 </div>
+
+<style>
+    .task-tile {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-areas:
+            "checkbox expand"
+            "name name"
+            "expandable expandable";
+        row-gap: 1.5rem;
+        column-gap: 1rem;
+        align-items: center;
+
+        padding-inline: 1.5rem;
+        padding-block-start: 1rem;
+    }
+
+    /* Dividing line */
+
+    .task-tile:where(:not(:last-child)) {
+        border-bottom: 1px solid var(--color-primary-200);
+    }
+
+    .task-tile__checkbox {
+        grid-area: checkbox;
+
+        width: 1rem;
+        aspect-ratio: 1;
+        border: 1px solid var(--color-primary-400);
+    }
+
+    .task-tile__name {
+        grid-area: name;
+
+        font-size: var(--font-size-300);
+
+        text-wrap: balance;
+    }
+
+    .expand {
+        grid-area: expand;
+        justify-self: end;
+    }
+
+    .expand__icon {
+        width: 100%;
+        height: 100%;
+    }
+
+    /* Expandable */
+
+    .task-tile__expandable {
+        grid-area: expandable;
+
+        padding-block-end: 1rem;
+
+        display: grid;
+        gap: 1.5rem;
+    }
+
+    .task-tile__description {
+        font-size: var(--font-size-100);
+    }
+
+    .controls {
+        display: grid;
+        grid-template-columns: 1fr auto auto;
+        gap: 1.5rem;
+    }
+
+    .controls__button {
+        height: 2rem;
+
+        box-shadow: 0.25rem 0.25rem 2rem 0.25rem rgba(0, 0, 0, 0.1);
+    }
+
+    .controls__icon {
+        width: 80%;
+        height: 80%;
+    }
+
+    .controls__button--give-up {
+        justify-self: start;
+
+        padding-inline: 1rem;
+
+        font-size: var(--color-primary-300);
+    }
+
+    .controls__button--edit,
+    .controls__button--delete {
+        aspect-ratio: 1;
+    }
+
+    @media screen and (width > 40rem) {
+        .task-tile {
+            padding-inline: 2.5rem;
+            padding-block-start: 2rem;
+
+            grid-template-columns: 1.5rem 1fr 2rem;
+            grid-template-areas:
+                "checkbox name expand"
+                ". expandable .";
+            row-gap: 2rem;
+            column-gap: 1.5rem;
+        }
+
+        .task-tile__checkbox {
+            width: 1.5rem;
+        }
+
+        .task-tile__name {
+            font-size: var(--font-size-400);
+        }
+
+        .expand {
+            width: 2rem;
+        }
+
+        .task-tile__expandable {
+            padding-block-end: 2rem;
+
+            gap: 2rem;
+        }
+
+        .task-tile__description {
+            font-size: var(--font-size-300);
+        }
+
+        .controls {
+            gap: 2rem;
+        }
+
+        .controls__button {
+            height: 3rem;
+        }
+
+        .controls__button--give-up {
+            font-size: var(--font-size-400);
+        }
+    }
+
+    @media screen and (width > 64rem) {
+        .task-tile {
+            padding-block-start: 2.5rem;
+            padding-inline: 3rem;
+
+            grid-template-columns: 2rem 1fr 3rem;
+
+            row-gap: 2.5rem;
+            column-gap: 3rem;
+        }
+
+        .task-tile__checkbox {
+            width: 2rem;
+        }
+
+        .task-tile__name {
+            font-size: var(--font-size-500);
+        }
+
+        .expand {
+            width: 3rem;
+        }
+
+        .task-tile__expandable {
+            padding-block-end: 2.5rem;
+            gap: 3rem;
+        }
+    }
+</style>
